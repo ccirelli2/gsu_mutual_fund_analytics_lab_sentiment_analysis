73b44ff8686ccdebb5c3d87a454e560a04dd1aff
Born2Ride <30387055+ccirelli2@users.noreply.github.com>
2021-04-24 09:32:17 -0400

    Delete deprecated_code directory

--- deprecated_code/scripts/groundtruth.py
+++
@@ -1,8 +0,0 @@
-
-import os
-import pandas as pd
-
-dir_repo = r'/home/cc2/Desktop/repositories/mutual_fund_analytics_lab_sentiment_analysis'
-dir_data = os.path.join(dir_repo, 'data')
-filename = r'paragraphs_sentiment_analysis.csv'
-data = pd.read_csv(os.path.join(dir_data, filename))
